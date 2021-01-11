93840e426ccebdbf2ec1e62ac1f0285a5a7f66da
SweAye <60157058+SweAye@users.noreply.github.com>
2021-01-10 21:23:56 -0800

    Update Read_Json_File.py

--- Code_Data/Read_Json_File.py
+++ Code_Data/Read_Json_File.py
@@ -26,40 +26,16 @@ for line in tweets_file:
         continue
 
     tweets = pd.DataFrame(tweets_data)
-
-
-
-
 tweets = pd.DataFrame(tweets_data)
 print(tweets.info())
-
-#tweets.to_csv('tweets.cvs')
-
 df = pd.read_csv('tweets.cvs')
-
 chosen_Df = pd.DataFrame(columns=['Date','Tweet','favorite_count'])
-
 print (chosen_Df.info())
 # sending data to the new dataframe
-
 chosen_Df[['Date','Tweet','favorite_count']] = df[['created_at','text','favorite_count']]
-
 # here Date is replacing with formated date
 chosen_Df['Date'] = tweet_day
-
 chosen_Df.to_csv('ReadyTweet.cvs')
 print(chosen_Df)
 print(chosen_Df['Tweet'])
 print (chosen_Df.info())
-
-
-
-
-
-
-#print (tweets['created_at'].values)
-
-#print (tweets['text'].values)
-
-
-
