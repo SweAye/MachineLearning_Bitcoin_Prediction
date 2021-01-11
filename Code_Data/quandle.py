631f3932720f8a3cb8c3f6436971638d0b9f0634
SweAye <60157058+SweAye@users.noreply.github.com>
2021-01-10 21:38:01 -0800

    Update quandle.py

--- Code_Data/quandle.py
+++ Code_Data/quandle.py
@@ -9,37 +9,37 @@ To change the requre start data and end datae go to this link
 https://www.quandl.com/data/BITFINEX/VETBTC-VET-BTC-Exchange-Rate
 
 """
-# r = requests.get('https://www.quandl.com/api/v3/datasets/BITFINEX/VETBTC.csv?api_key=""')
-# print(r)
-# #sending jason file into the dat frame
-# df = pd.DataFrame(r)
-# df.to_csv('quandle.csv')
-# print (df.info())
-# print(df.head())
-# drop the null values
-# df.dropna(inplace=True)
-#
-# #split a cloumn to two column
-#
-# #since "bpi" is one columns with values and key dictionary type
-#
-# # sending values of the bpi as price
-# Price = df['bpi'].values
-# print (Price)
-#
-# # sending key (Dates) of the bpi as Date
-# Date= df['bpi'].keys()
-# print (Date)
-#
-# # making a new data frame with columns labels
-# newDf = pd.DataFrame(columns=['Date','Close'])
-# newDf['Date']= Date # fill the column with Date
-# newDf['Close']= Price# fill the column with Prices
+r = requests.get('https://www.quandl.com/api/v3/datasets/BITFINEX/VETBTC.csv?api_key=""')
+print(r)
+#sending jason file into the dat frame
+df = pd.DataFrame(r)
+df.to_csv('quandle.csv')
+print (df.info())
+print(df.head())
+drop the null values
+df.dropna(inplace=True)
 
-#newDf.to_csv('coindesk.csv')
-# df = pd.read_csv('quandle.csv',parse_dates=True,index_col=0 )
-# df = df.round(4)
-# print(df.head())
+#split a cloumn to two column
+
+#since "bpi" is one columns with values and key dictionary type
+
+# sending values of the bpi as price
+Price = df['bpi'].values
+print (Price)
+
+# sending key (Dates) of the bpi as Date
+Date= df['bpi'].keys()
+print (Date)
+
+# making a new data frame with columns labels
+newDf = pd.DataFrame(columns=['Date','Close'])
+newDf['Date']= Date # fill the column with Date
+newDf['Close']= Price# fill the column with Prices
+
+newDf.to_csv('coindesk.csv')
+df = pd.read_csv('quandle.csv',parse_dates=True,index_col=0 )
+df = df.round(4)
+print(df.head())
 main_api = 'https://www.quandl.com/api/v3/datasets/BITSTAMP/USD.json?api_key=""'
 
 json_data = requests.get(main_api).json()
@@ -48,17 +48,17 @@ json_status = json_data['dataset']
 print ("Json status: ", json_status)
 formatted_address = json_data['dataset']['id']
 print ("\nformatted_address:", formatted_address)
-#df = pd.DataFrame(r, columns=['bpi'])
+df = pd.DataFrame(r, columns=['bpi'])
 
-#data = r.dataset.dataset_code
-#print (data)
+data = r.dataset.dataset_code
+print (data)
 
-# df = pd.DataFrame(r, columns= ['column_names'])
-# df.to_csv('quandle.csv')
-# print (df.info())
+df = pd.DataFrame(r, columns= ['column_names'])
+df.to_csv('quandle.csv')
+print (df.info())
 
-#sending jason file into the dat frame
-# df = pd.DataFrame(r)
-# df.to_csv('quandle.csv')
-# print (df.info())
-# print(df.head())
+sending jason file into the dat frame
+df = pd.DataFrame(r)
+df.to_csv('quandle.csv')
+print (df.info())
+print(df.head())
