8751822a49c753d8a47ccf9b14512cf902831ba1
SweAye <60157058+SweAye@users.noreply.github.com>
2021-01-10 21:36:31 -0800

    Update coinbase_bitcoin.py

--- Code_Data/coinbase_bitcoin.py
+++ Code_Data/coinbase_bitcoin.py
@@ -33,8 +33,6 @@ df.dropna(inplace=True)
 
 #split a cloumn to two column
 
-#since "bpi" is one columns with values and key dictionary type
-
 # sending values of the bpi as price
 Price = df['bpi'].values
 #print (Price)
@@ -49,10 +47,6 @@ newDf['Date']= Date # fill the column with Date
 newDf['Close']= Price# fill the column with Prices
 
 newDf.to_csv('coindesk.csv')
-#---------------new code from Test.py below------------------
-
-
-
 df = pd.read_csv('coindesk.csv')
 df = df.set_index('Date')
 print (df.info())
@@ -65,89 +59,56 @@ df.drop(["a"], axis=1, inplace=True)
 
 print (df.head())
 
-#df.plot(kind= 'box',subplots=True, layout= (1,6),sharex=False,sharey=False)
-# df['Close'].plot()
-# plt.title("CoinDesk bitcoin price from 2016January To 2019January")
-# plt.xlabel("Date")
-# plt.ylabel("Price")
-#plt.show()
-
-
+df.plot(kind= 'box',subplots=True, layout= (1,6),sharex=False,sharey=False)
+df['Close'].plot()
+plt.title("CoinDesk bitcoin price from 2016January To 2019January")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
 
 
 forecast_col = 'Close'
-# # # get the data set length percentage's 0.1 will be in the forecasted
+# get the data set length percentage's 0.1 will be in the forecasted
 forecast_out = int(math.ceil(0.1* len(df)))# 1 % of the data
-#
-# # preparaing for the empty labels for the incoming forcast
 #
 df['label']= df[forecast_col].shift(-forecast_out)
 print (df)
 #
-#
 df.dropna(inplace=True)
-# #
+# 
 print (df.tail())
-
-
-
 x = np.array(df.drop(['Close'],1)) # all columns, other than label column
 y = np.array(df['Close'])# only label column
-
-#
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 
 # Linear regression model
-# linear_regression = LinearRegression()
-# linear_regression.fit(x_train,y_train)
+linear_regression = LinearRegression()
+linear_regression.fit(x_train,y_train)
 
 # # ---------Pickling-----------
-#
-# # with open ('linearregressionCoinDeskFitted.pickle', 'wb') as f:
-# #     pickle.dump(linear_regression,f)
 
-
+ with open ('linearregressionCoinDeskFitted.pickle', 'wb') as f:
+ pickle.dump(linear_regression,f)
 pickled = open('linearregressionCoinDeskFitted.pickle','rb')
 linear_regression = pickle.load(pickled)
-
-
-
-
 LinearRegression(copy_X= True, fit_intercept=True,n_jobs=1, normalize=False)
 accuracy = linear_regression.score(x_test,y_test)
-# # #
-print ('\nLinear_regression accuracy is :', accuracy,"\n") # only 36% accurency
-
-
-# # # New predit
-#
 X = x[:-forecast_out]
-#
-# # # old one
-#
 X_lately = x[-forecast_out:]
-
 
 Forecast_set = linear_regression.predict(X_lately)
 print (Forecast_set)
-
-# # just visualization
 
 df['Forecast'] = np.nan
 
 last_date = df.iloc[-1].name
 
-# print ("last date is: " , last_date)
 last_date = time.mktime(datetime.datetime.strptime(last_date,"%Y-%m-%d").timetuple())
 
 one_day = 86400
 next_unix = last_date + 86400
 
-
-## just to show the forcast_set with Price values
 label_arry = np.array(df['label'])
-
-
 next_date_array= []
 # # just visualization ( later get inside the
 for i in Forecast_set:
@@ -160,37 +121,11 @@ for i in Forecast_set:
     next_date_array.append(next_date) # just to get an arrray for later use
     next_unix +=one_day
     df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] +[i]
-
-
 print ("next_date array is: ", next_date_array)
-
 # make a forcast vs nexdate dataset for Demo
-
 newDf = pd.DataFrame(columns=['Date','Price'])
-
-
 newDf = pd.DataFrame({'Date': next_date_array[:],'Price': Forecast_set[:]})
 
-print ("newDf is: ---->", newDf)
-
-
-# let's do the same dataframe
-
-
-# plt.plot(newDf['Price'])
-#
-# plt.title("Forcasted Price vs Original Price graph")
-# plt.xlabel("Date")
-# plt.ylabel("Prices")
-# plt.show()
-# print (len(next_date_array))
-# print (len(Forecast_set))
-
-#new_df['Date']= next_date_array
-#new_df['Price']= Forecast_set
-# print (new_df)
-
-#------------------------------------------------------
 df['Close'].plot()
 df['Forecast'].plot()
 
